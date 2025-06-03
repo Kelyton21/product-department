7d9039f1aa9ce92157436e2e38279e9f4fb4b9a9
@Kelyton21 <kelyton2004@gmail.com>
2025-06-03 00:18:11 -0300

    Controller adicicionado

---
+++ produto_departamento/src/main/java/com/devsuperior/produto_departamento/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.produto_departamento.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.produto_departamento.dto.ProductDTO;
+import com.devsuperior.produto_departamento.services.ProductService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping(value =  "/products")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+    @GetMapping
+    public List<ProductDTO> findAll() {
+        return productService.findAll();
+    }
+}
