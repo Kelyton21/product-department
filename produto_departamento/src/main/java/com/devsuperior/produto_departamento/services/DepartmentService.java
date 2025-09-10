781489af2efb7da896b25d8cc5972cc2ece14072
@Kelyton21 <kelyton2004@gmail.com>
2025-09-09 21:01:36 -0300

    refactor(service): Adiciona Transactional para garantir ACID

--- produto_departamento/src/main/java/com/devsuperior/produto_departamento/services/DepartmentService.java
+++ produto_departamento/src/main/java/com/devsuperior/produto_departamento/services/DepartmentService.java
@@ -9,12 +9,15 @@ import com.devsuperior.produto_departamento.dto.DepartmentDTO;
 import com.devsuperior.produto_departamento.entities.Department;
 import com.devsuperior.produto_departamento.repositories.DepartmentRepository;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository departmentRepository;
-    
+
+    @Transactional
     public Long createDepartment(DepartmentDTO departmentDTO) {
         // DTO -> Entity
         var entity = new Department(null, departmentDTO.getName());
@@ -22,6 +25,7 @@ public class DepartmentService {
         return departmentSave.getId();
     }
 
+    @Transactional
     public DepartmentDTO update(Long id, DepartmentDTO departmentDTO) {
         
         var department = departmentRepository.findById(id)
@@ -38,12 +42,14 @@ public class DepartmentService {
         return new DepartmentDTO(updatedDepartment.getName());
     }
 
+    @Transactional
     public void delete(Long id) {
         var department = departmentRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Department not found with id: " + id));
         departmentRepository.delete(department);
     }
-
+    
+    @Transactional(readOnly = true)
     public List<DepartmentDTO> findAll() {
         List<Department> departments = departmentRepository.findAll();
         List<DepartmentDTO> departmentDTOs = departments.stream()
